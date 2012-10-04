6ca4d8899221a7ff682636f66344844db24cdb59
kulppi <mkulppi@gmail.com>
2012-10-04 11:08:46 -0300

    arregar records

--- db/migrate/20121002202631_rename_marca_from_registro_records.rb
+++ db/migrate/20121002202631_rename_marca_from_registro_records.rb
@@ -1,7 +1,7 @@
 class RenameMarcaFromRegistroRecords < ActiveRecord::Migration
   def up
 
-    remove_column :registro_records, :marca, :integer
+    remove_column :registro_records, :marca
     add_column :registro_records, :marca, :float
   end
 
